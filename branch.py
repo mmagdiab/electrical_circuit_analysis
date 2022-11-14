68bb74bb9a7202e3eca49a2390891c122b6e6763
Mohamed Magdy <14momagdi@gmail.com>
2022-11-14 13:59:15 +0200

    fixes to ui and add integration

--- branch.py
+++ branch.py
@@ -1,5 +1,6 @@
 class Branch(object):
-    def __init__(self, starting_node, ending_node, voltage_source=0, current_source=0, resistance=0):
+    def __init__(self, sequence, starting_node, ending_node, voltage_source=0, current_source=0, resistance=0):
+        self.sequence = sequence
         self.starting_node = starting_node
         self.ending_node = ending_node
         self.voltage_source = voltage_source
@@ -7,4 +8,11 @@ class Branch(object):
         self.resistance = resistance
 
     def __str__(self):
-        return 'Branch: Starting node: ' + str(self.starting_node) + ' ,Ending node: ' + str(self.ending_node)
+        return 'Branch ' + chr(65+self.sequence) + ': ' +\
+               'Starting node: ' + str(self.starting_node) +\
+               ' ,Ending node: ' + str(self.ending_node) + \
+               ' ,Voltage Source: ' + str(self.voltage_source) + \
+               ' ,Current Source: ' + str(self.current_source) + \
+               ' ,Resistance: ' + str(self.resistance)
+
+
--- main.py
+++ main.py
@@ -1,69 +1,16 @@
-from utils import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 from branch import *
-
-import networkx as nx
-import matplotlib.pyplot as plt
-
-# Create a Directed Graph
-G = nx.DiGraph()
+from ui import *
 
 
-def test_tree():
-    a = Branch(3, 1)
-    b = Branch(1, 2)
-    c = Branch(2, 3)
-    d = Branch(4, 1)
-    e = Branch(2, 4)
-    f = Branch(3, 4)
-    Jb = [23,54,766,34,45,2]
-    Vb = [23,4,56,7,554,132]
-    circuit_branches = [a, b, c, d, e, f]
+def main():
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = QtWidgets.QMainWindow()
+    Ui(main_window)
+    main_window.show()
+    sys.exit(app.exec_())
 
-    tree_branches, links = tree(circuit_branches)
-    
-    for branch in tree_branches:
-        print(branch)
-    print('0000000000000000000-------------------0000000000000000')
-    for branch in links:
-        print(branch)
 
-        
-    #Count Nodes and add these nodes to the graph
-    nodeList = []
-    for node in tree_branches:
-        nodeList.append(node.starting_node)
-        nodeList.append(node.ending_node)
-        G.add_node(node.starting_node)
-        G.add_node(node.ending_node)
-    
-    # remove any douplicated nodes
-    nodeList = set(nodeList)
-    
-    
-    #edges (Branch data) contains starting, ending, current in branch, voltage on branch
-    edgesList = []
-    for i in tree_branches:
-        #These str(2), str(3) should be replaced by the its corresponding values in the Voltage and Current matrices
-        edgesList.append((i.starting_node, i.ending_node, str(2)+" mA, " + str(3)+" V" ))
-    
-    
-    for i in links:
-        #These str(4), str(5) should be replaced by the its corresponding values in the Voltage and Current matrices
-        edgesList.append((i.starting_node, i.ending_node, str(4)+" mA, " + str(5)+" V" ))
-    
-    
-    for i in edgesList:
-        G.add_edge(i[0], i[1],edgesList=i[2])
-    
-    print("Node List :",G.nodes())
-    print("Edge List :",G.edges())
-    
-    pos = nx.spring_layout(G)
-    colors = range(20)
-    nx.draw(G,alpha=1, with_labels = True, node_color= 'green')     
-    #nx.draw(G,pos, edge_color = colors,width =4, edge_cmap= plt.cm.Blues)
-    nx.draw_networkx_edge_labels(G,pos, font_size=15,edge_labels=nx.get_edge_attributes(G,'edgesList'))
-    
-    plt.show()
 if __name__ == '__main__':
-    test_tree()
+    main()
